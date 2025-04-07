d1e4327d09cc9bb17966b86da6283c6014fb25cc
xXerii <52268893+xXerii@users.noreply.github.com>
2025-04-07 12:20:26 +0800

    Deleted CodeRunner
    might come up again

--- CleanConnect/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-    def loginAccount(self, username, password):
-        cursor = db.cursor()
-        query = "SELECT * FROM useraccounts WHERE username = %s AND password = %s"
-        cursor.execute(query, (username, password))
